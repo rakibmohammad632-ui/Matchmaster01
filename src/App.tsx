8b820e259eb428ea4a2386822662b4b1845c9244
rakibmohammad632-ui <rakibmohammad632@gmail.com>
2026-05-22 04:50:55 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import BackgroundParticles from "./components/BackgroundParticles";
 import { motion, AnimatePresence } from 'motion/react';
 import { Smartphone, CheckCircle, Settings, HelpCircle, Loader } from 'lucide-react';
 
